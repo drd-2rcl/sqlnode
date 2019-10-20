b97f6414b08b99ac0fbe7ab90b0457e498fd5cb6
Diogo <drd.rclrd@gmail.com>
2019-10-20 19:36:09 -0300

    config for sequelize and connection db

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'docker',
+  password: 'docker',
+  database: 'sqlnode',
+  defined: {
+    timestamps: true,
+    underscored: true,
+  },
+}
